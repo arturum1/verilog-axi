dacaa973317fad246ae3422e2f3af2dc4350a0de
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-05-26 23:45:11 +0100

    feat(axi_interconnect): Update axi_interconnect.py to a python class

--- rtl/axi_interconnect.py
+++ rtl/axi_interconnect.py
@@ -1,2 +1,17 @@
-headers     = [  ]
-modules    = [ 'priority_encoder.v', 'arbiter.v', 'axi_interconnect.v' ]
+from iob_module import iob_module
+
+class axi_interconnect(iob_module):
+    def __init__(self, **kwargs):
+        super().__init__(
+                name='axi_interconnect',
+                version='V0.10',
+                **kwargs
+                )
+
+    # This module accepts the following non-standard parameters:
+    # out_dir: Output directory for placement of source files during setup
+    def setup(self, out_dir="hardware/src", **kwargs):
+        super().setup(**kwargs)
+
+        self.headers = []
+        self.modules = [ 'priority_encoder.v', 'arbiter.v', 'axi_interconnect.v' ]
