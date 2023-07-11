8b6dbc0a838b61840f99b3dfdc32daf8430fac6a
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-07-12 00:15:06 +0100

    feat(_post_setup): Rename _run_setup method.

--- rtl/axi_interconnect.py
+++ rtl/axi_interconnect.py
@@ -10,8 +10,8 @@ class axi_interconnect(iob_module):
     setup_dir = os.path.dirname(__file__)
 
     @classmethod
-    def _run_setup(cls):
-        super()._run_setup()
+    def _post_setup(cls):
+        super()._post_setup()
 
         # Setup dependencies
 
