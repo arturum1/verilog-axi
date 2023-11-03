671e8defe96d85774fd712744cc41617f6da86b5
PMiranda <pmiranda.iobundle@gmail.com>
2023-11-03 15:42:37 +0000

    fix(modules): axi_ram and axi_interconnect modules - axi_ram and axi_interconnect use iob_module directory structure

---
+++ rtl/axi_interconnect/axi_interconnect.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+# Find python modules
+if __name__ == "__main__":
+    import sys
+
+    sys.path.append("./scripts")
+
+from iob_module import iob_module
+
+
+if __name__ == "__main__":
+    iob_module.find_modules()
+
+class axi_interconnect(iob_module):
+    @classmethod
+    def _init_attributes(cls):
+        """Init module attributes"""
+        cls.name = "axi_interconnect"
+        cls.version = "V0.10"
+        cls.setup_dir = os.path.dirname(__file__)
