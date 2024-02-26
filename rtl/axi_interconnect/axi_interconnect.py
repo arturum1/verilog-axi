489e9ab3a683d32de41b9bbb80b18b86122cbe82
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-02-26 16:34:01 +0000

    fix(__init__): Add missing super().__init__() call

--- rtl/axi_interconnect/axi_interconnect.py
+++ rtl/axi_interconnect/axi_interconnect.py
@@ -4,6 +4,7 @@ from iob_module import iob_module
 
 class axi_interconnect(iob_module):
     def __init__(self):
+        super().__init__()
         self.name = "axi_interconnect"
         self.version = "V0.10"
         self.setup_dir = os.path.dirname(__file__)
