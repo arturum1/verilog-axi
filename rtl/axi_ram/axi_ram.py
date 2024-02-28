a38e5686e20e16b5b71958efbad0bb6f6605cb87
PMiranda <pmiranda.iobundle@gmail.com>
2024-02-28 17:41:29 +0000

    fix(axi_ram): update axi_ram.py for if_gen2 - update axi_ram.py to work with lib/ axis2axi testbench

--- rtl/axi_ram/axi_ram.py
+++ rtl/axi_ram/axi_ram.py
@@ -1,9 +1,7 @@
-import os
-
 from iob_module import iob_module
+
 
 class axi_ram(iob_module):
     def __init__(self):
         super().__init__()
-        self.version='V0.10'
-        self.setup_dir=os.path.dirname(__file__)
+        self.version = "V0.10"
