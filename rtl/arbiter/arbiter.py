708388dcb7bc2c0c2caeb1ebf5d8f1b51b49d017
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-05-27 05:01:08 +0100

    feat(arbiter): Add dedicated arbiter module. Allows cores to use the arbiter module without axi_interconnect (like the iob-eth core).

--- rtl/arbiter/arbiter.py
+++ rtl/arbiter/arbiter.py
@@ -7,26 +7,16 @@ if __name__ == "__main__":
     sys.path.append("./scripts")
 
 from iob_module import iob_module
-from arbiter import arbiter
 
 
 if __name__ == "__main__":
     iob_module.find_modules()
 
 
-class axi_interconnect(iob_module):
+class arbiter(iob_module):
     @classmethod
     def _init_attributes(cls):
         """Init module attributes"""
-        cls.name = "axi_interconnect"
+        cls.name = "arbiter"
         cls.version = "V0.10"
         cls.setup_dir = os.path.dirname(__file__)
-
-    @classmethod
-    def _create_submodules_list(cls):
-        """Create submodules list with dependencies of this module"""
-        super()._create_submodules_list(
-            [
-                arbiter,
-            ]
-        )
